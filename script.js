13e9d8c6b057046a8f76b0b762463b19598183a6
JasonAdder <161970590+JasonAdder@users.noreply.github.com>
2024-03-03 11:00:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // Check if the search query is not empty
         if (searchQuery) {
-            // Construct the URL for the search results page
-            const searchUrl = 'https://www.google.com/search?sca_esv=d0136fcaccc01fe6&q=food&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjc_rSPrNiEAxVkVTABHURwBGQQ0pQJegQIDRAB&biw=719&bih=782&dpr=1.25&q=' + encodeURIComponent(searchQuery);
+            // Construct the URL for the Google Images search results page
+            const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery) + '&tbm=isch';
 
-            // Redirect the user to the search results page
+            // Redirect the user to the Google Images search results page
             window.location.href = searchUrl;
         }
+    });
+
+    // Get the switch element
+    const switchElement = document.getElementById('toggleTheme');
+
+    // Add event listener for switch change
+    switchElement.addEventListener('change', function() {
+        // Toggle dark mode class on the body
+        document.body.classList.toggle('dark-mode');
     });
 });
